a3403585ebe11e2bd6527baf1c7066efc5a02405
jaeyeoneej <5ikve@naver.com>
2022-10-26 21:41:39 +0900

    Revert "Revert "키워드: 헤더  #5""
    This reverts commit f05047a1f390c16bd86d00ac8d965002c35e5e9a.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Header from "../components/header/Header";
 import Layout from "../components/layout/Layout";
 // import HomeMenu from "../components/homeMenu/HomeMenu";
 import PostList from "../components/postList/PostList";
@@ -6,6 +7,7 @@ import PostList from "../components/postList/PostList";
 const Home = () => {
   return (
     <>
+      <Header />
       <Layout>
         <PostList />
       </Layout>
